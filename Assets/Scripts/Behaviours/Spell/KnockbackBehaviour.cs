56331a2e7586dc7a296454df8f62ea4b5ca825c4
Ramon <ramon@mon>
2015-06-05 05:21:22 +0300

    modified a little the knockback spell behaviour

--- Assets/Scripts/Behaviours/Spell/KnockbackBehaviour.cs
+++ Assets/Scripts/Behaviours/Spell/KnockbackBehaviour.cs
@@ -36,7 +36,9 @@ public class KnockbackBehaviour : SpellBehaviour
 				continue;
 
 			float knockbackDistance = Vector3.Distance(transform.position, coll.transform.position);
-			Vector3 knockbackForce = (coll.transform.position - transform.position).normalized * SpellConstants.KnockbackForce * (2 - knockbackDistance);
+			Vector3 knockbackForce = (coll.transform.position - transform.position).normalized * SpellConstants.KnockbackForce * (2.5f - knockbackDistance);
+			Debug.Log(knockbackDistance);
+			Debug.Log("2.4 - ^: " + (2.5 - knockbackDistance));
 			ServerManager.ApplyDamageWithForce(entity, coll.GetComponent<Survivor>(), SpellConstants.KnockbackDamage, knockbackForce);
 		}
 	}
--- Assets/Scripts/Constants/Spell/SpellConstants.cs
+++ Assets/Scripts/Constants/Spell/SpellConstants.cs
@@ -8,8 +8,8 @@ namespace Warlop.Constants
 		public const float FireballDamage = 10f;
 		public const float FireballTorque = 400f;
 
-		public const float KnockbackRate = 4f;
-		public const float KnockbackRadius = 1.5f;
+		public const float KnockbackRate = 0f;
+		public const float KnockbackRadius = 2f;
 		public const float KnockbackForce = 5f;
 		public const float KnockbackDamage = 5f;
 		public const float KnockbackChanneling = 1.5f;
